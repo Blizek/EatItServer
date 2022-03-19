062eb60354c3ec92ad61ebc4a12790e7bfaeac94
Blizek <blazej.naziemiec@gmail.com>
2022-03-19 14:28:34 +0100

    Initial commit

---
+++ src/main/java/eat_it_server/EatItServerApplication.java
@@ -0,0 +1,13 @@
+package eat_it_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatItServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatItServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eat_it_server/EatItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eat_it_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
